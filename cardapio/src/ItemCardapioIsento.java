9107c02c1821539c163c910cf2aff28b11fc5c91
Luis <martinsluisdev@gmail.com>
2026-09-03 15:53:25 -0300

    feat(ItemCardapioIsento): cria classe que é filha de ItemCardapio e sobrescreve metodo calculaImposto

---
+++ cardapio/src/ItemCardapioIsento.java
@@ -0,0 +1,11 @@
+public class ItemCardapioIsento extends ItemCardapio{
+
+    public ItemCardapioIsento(long id, String nome, String descricao, double preco, CategoriaCardapio categoria) {
+        super(id, nome, descricao, preco, categoria);
+    }
+
+    @Override
+    double calculaIposto() {
+        return 0.0;
+    }
+}
