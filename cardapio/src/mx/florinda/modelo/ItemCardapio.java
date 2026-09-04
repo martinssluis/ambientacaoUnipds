c513b8fc8c9bd481141d9782491ec442beecab53
Luis <martinsluisdev@gmail.com>
2026-09-04 10:49:48 -0300

    refactor(ItemCardapio): muda atributos para private, cria getter e setter, e move arquivo para diretorio dedicado

---
+++ cardapio/src/mx/florinda/modelo/ItemCardapio.java
@@ -0,0 +1,74 @@
+package mx.florinda.modelo;
+
+public class ItemCardapio {
+
+    // atributos
+    private long id;
+    private String nome;
+    private String descricao;
+    private boolean emPromocao;
+    private double preco;
+    private double precoComDesconto;
+    private CategoriaCardapio categoria;
+
+    //construtor
+    public ItemCardapio(long id, String nome, String descricao, double preco, CategoriaCardapio categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    //metodos
+    public double getPorcentagemDesconto() {
+        return (preco - precoComDesconto) / preco * 100;
+    }
+
+    public CategoriaCardapio obtemNomeCategoria() {
+        return categoria;
+    }
+
+    public void setPromocao(double precoComDesconto) {
+        emPromocao = true;
+        this.precoComDesconto = precoComDesconto;
+    }
+
+    public double getImposto() {
+        double imposto;
+        if (emPromocao) {
+            imposto = precoComDesconto * 0.1;
+        } else {
+            imposto = preco * 0.1;
+        }
+        return imposto;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isEmPromocao() {
+        return emPromocao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPrecoComDesconto() {
+        return precoComDesconto;
+    }
+
+    public CategoriaCardapio getCategoria() {
+        return categoria;
+    }
+}
