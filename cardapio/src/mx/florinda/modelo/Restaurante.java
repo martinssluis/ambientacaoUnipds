27d791c7f5cf17c2263b50fd67e142bb8492419e
Luis <martinsluisdev@gmail.com>
2026-09-04 14:15:26 -0300

    feat(Restaurante): adiciona atributo Cardapio

--- cardapio/src/mx/florinda/modelo/Restaurante.java
+++ cardapio/src/mx/florinda/modelo/Restaurante.java
@@ -3,10 +3,12 @@ package mx.florinda.modelo;
 public class Restaurante {
     private String nome;
     private String endereco;
+    private Cardapio cardapio;
 
-    public Restaurante(String nome, String endereco) {
+    public Restaurante(String nome, String endereco, Cardapio cardapio) {
         this.nome = nome;
         this.endereco = endereco;
+        this.cardapio = cardapio;
     }
 
     public String getNome() {
@@ -15,5 +17,9 @@ public class Restaurante {
 
     public String getEndereco() {
         return endereco;
+    }
+
+    public Cardapio getCardapio() {
+        return cardapio;
     }
 }
