29dc74a86483956b693fb6000de0347d86156b03
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-06 20:59:34 -0300

    feat(Main): solicita nome do arquivo de cardápio ao usuário

--- cardapio/src/mx/florinda/cli/Main.java
+++ cardapio/src/mx/florinda/cli/Main.java
@@ -1,10 +1,11 @@
 import mx.florinda.modelo.Cardapio;
 import mx.florinda.modelo.ItemCardapio;
 
-void main() {
+void main() throws IOException{
 
-    Cardapio cardapio = new Cardapio();
+    String nomeArquivo = IO.readln("Digite um nome de arquivo de itens de cardapio: ");
 
+    Cardapio cardapio = new Cardapio(nomeArquivo);
     String linha = IO.readln("Digite um id de um item de cardápio: ");
     long idSelecionado = Long.parseLong(linha);
 
