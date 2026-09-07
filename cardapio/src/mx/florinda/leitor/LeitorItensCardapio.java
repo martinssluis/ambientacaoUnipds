6593922a95d3333b333bb0a6337ddad7e261adcf
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-07 16:41:35 -0300

    feat(LeitorItensCarcapio): cria interface com metodo processarArquivo

---
+++ cardapio/src/mx/florinda/leitor/LeitorItensCardapio.java
@@ -0,0 +1,11 @@
+package mx.florinda.leitor;
+
+import mx.florinda.modelo.ItemCardapio;
+
+import java.io.IOException;
+
+public interface LeitorItensCardapio {
+
+    ItemCardapio[] processaArquivo(String nomeArquivo) throws IOException;
+
+}
