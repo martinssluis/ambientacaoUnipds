478474d6bf1a646e1f00b01e2c53b022450056db
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-07 16:44:15 -0300

    feat(FabricaLeitorItensCardapio): cria sabrica que dependendo da extensão do arquivo, cria uma instância diferente

---
+++ cardapio/src/mx/florinda/leitor/FabricaLeitorItensCardapio.java
@@ -0,0 +1,20 @@
+package mx.florinda.leitor;
+
+public class FabricaLeitorItensCardapio {
+
+    public LeitorItensCardapio criaLeitor(String nomeArquivo){
+
+        LeitorItensCardapio leitor = null;
+
+        if (nomeArquivo.endsWith(".csv")) {
+
+            leitor = new LeitorItensCardapioCSV();
+
+        } else if (nomeArquivo.endsWith(".json")) {
+
+            leitor = new LeitorItensCardapioJSON();
+
+        }
+        return leitor;
+    }
+}
