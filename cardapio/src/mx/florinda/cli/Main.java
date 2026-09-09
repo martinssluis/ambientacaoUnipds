12958b744e90bb146cdd13086eca5deb54bb6183
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 18:00:18 -0300

    feat(Main): atualiza classe de teste para exercitar Relatorio, RelatorioSimples e RelatorioDetalhado

--- cardapio/src/mx/florinda/cli/Main.java
+++ cardapio/src/mx/florinda/cli/Main.java
@@ -1,47 +1,17 @@
-import mx.florinda.modelo.Cardapio;
-import mx.florinda.modelo.ItemCardapio;
+import mx.florinda.Relatorio.Relatorio;
+import mx.florinda.Relatorio.RelatorioDetalhado;
+import mx.florinda.Relatorio.RelatorioSimples;
 
-void main() throws IOException{
 
-    String nomeArquivo = IO.readln("Digite um nome de arquivo de itens de cardapio: ");
+void main(){
 
-    Cardapio cardapio = new Cardapio(nomeArquivo);
-    String linha = IO.readln("Digite um id de um item de cardápio: ");
-    long idSelecionado = Long.parseLong(linha);
+    Relatorio relatorio1 = new RelatorioSimples();
+    relatorio1.gerarRelatorio();
 
-    ItemCardapio itemSelecionado = cardapio.getItemPorId(idSelecionado);
+    IO.println("----------");
+    Relatorio relatorio2 = new RelatorioDetalhado();
+    relatorio2.gerarRelatorio();
 
-    IO.println("== Item do Cardápio ==");
-    IO.println("Id: " + itemSelecionado.getId());
-    IO.println("Nome: " + itemSelecionado.getNome());
-    IO.println("Descrição: " + itemSelecionado.getDescricao());
-    if (itemSelecionado.isEmPromocao()) {
-        IO.println("Item em promoção! 🤑");
-        double porcentagemDesconto = itemSelecionado.getPorcentagemDesconto();
-        IO.println("Preco: de " + itemSelecionado.getPreco() + " por " + itemSelecionado.getPrecoComDesconto());
-        IO.println("Porcentagem de desconto: " + porcentagemDesconto);
-    } else {
-        IO.println("Preco: " + itemSelecionado.getPreco());
-        IO.println("Item não está em promoção");
-    }
-    IO.println("Categoria: " + itemSelecionado.getCategoria());
-    IO.println("Imposto: " + itemSelecionado.getImposto());
-
-    IO.println("-------");
-
-    IO.println("Soma dos preços: " + cardapio.getSomaDosPrecos());
-    IO.println("Total de itens em promoção: " + cardapio.getTotalDeItensEmPromocao());
-
-    double precoLimite = 10.0;
-    IO.println("O primeiro preço que é maior que " + precoLimite + ": " + cardapio.getPrimeiroPrecoMaiorQueLimite(precoLimite));
-
-    IO.println("-------");
-
-    // Imprimir todos os precos menores ou iguais ao limite
-    for (ItemCardapio item : cardapio.getItens()) {
-        if (item.getPreco() <= precoLimite) {
-            IO.println("Preço menor que " + precoLimite + ": " + item.getPreco());
-        }
-    }
+    Relatorio.imprimirDataHora();
 
 }
