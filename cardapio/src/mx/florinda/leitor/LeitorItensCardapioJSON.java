d6c2a5aa73919977f7788a09acd9afbbd7b3732a
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 14:05:00 -0300

    feat(LeitorIntensCardapioJSON): passa a extender classe abstrata LeitorItensCardapioBase, instanciando o metodo concreto processaLinha de acordo com sua variação

--- cardapio/src/mx/florinda/leitor/LeitorItensCardapioJSON.java
+++ cardapio/src/mx/florinda/leitor/LeitorItensCardapioJSON.java
@@ -5,82 +5,71 @@ import mx.florinda.modelo.ItemCardapio;
 import mx.florinda.modelo.isento.ItemCardapioIsento;
 
 import java.io.IO;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-public class LeitorItensCardapioJSON implements LeitorItensCardapio{
+
+public class LeitorItensCardapioJSON extends LeitorItensCardapioBase {
+
+    public LeitorItensCardapioJSON(String nomeArquivo) {
+        super(nomeArquivo);
+    }
 
     @Override
-    public ItemCardapio[] processaArquivo(String nomeArquivo) throws IOException {
+    protected ItemCardapio processaLinha(String linha) {
+        linha = linha.replace("[", "");
+        linha = linha.replace("]", "");
+        linha = linha.replace("{", "");
+        linha = linha.replace("}", "");
+        linha = linha.replace("\"", "");
 
-        Path arquivo = Path.of(nomeArquivo);
-        String conteudoDoArquivo = Files.readString(arquivo);
-        String[] linhasArquivo = conteudoDoArquivo.split("\n");
+        String[] partes = linha.split(",");
 
-        ItemCardapio[] itens = new ItemCardapio[linhasArquivo.length];
+        String parteId = partes[0];
+        String[] propriedadeEValorId = parteId.split(":");
+        String valorId = propriedadeEValorId[1].trim();
+        long id = Long.parseLong(valorId);
+        IO.println(id);
 
-        for (int i = 0; i < linhasArquivo.length; i++) {
-            String linha = linhasArquivo[i];
+        String parteNome = partes[1];
+        String[] propriedadeEValorNome = parteNome.split(":");
+        String nome = propriedadeEValorNome[1].trim();
 
+        String parteDescricao = partes[2];
+        String[] propriedadeEValorDescricao = parteDescricao.split(":");
+        String descricao = propriedadeEValorDescricao[1].trim();
 
-            linha = linha.replace("[", "");
-            linha = linha.replace("]", "");
-            linha = linha.replace("{", "");
-            linha = linha.replace("}", "");
-            linha = linha.replace("\"", "");
+        String partePreco = partes[3];
+        String[] propriedadeEValorPreco = partePreco.split(":");
+        String valorPreco = propriedadeEValorPreco[1].trim();
+        double preco = Double.parseDouble(valorPreco);
 
-            String[] partes = linha.split(",");
+        String parteCategoria = partes[4];
+        String[] propriedadeEValorCategoria = parteCategoria.split(":");
+        String valorCategoria = propriedadeEValorCategoria[1].trim();
+        CategoriaCardapio categoria = CategoriaCardapio.valueOf(valorCategoria);
 
-            String parteId = partes[0];
-            String[] propriedadeEValorId = parteId.split(":");
-            String valorId = propriedadeEValorId[1].trim();
-            long id = Long.parseLong(valorId);
-            IO.println(id);
-
-            String parteNome = partes[1];
-            String[] propriedadeEValorNome = parteNome.split(":");
-            String nome = propriedadeEValorNome[1].trim();
-
-            String parteDescricao = partes[2];
-            String[] propriedadeEValorDescricao = parteDescricao.split(":");
-            String descricao = propriedadeEValorDescricao[1].trim();
-
-            String partePreco = partes[3];
-            String[] propriedadeEValorPreco = partePreco.split(":");
-            String valorPreco = propriedadeEValorPreco[1].trim();
-            double preco = Double.parseDouble(valorPreco);
-
-            String parteCategoria = partes[4];
-            String[] propriedadeEValorCategoria = parteCategoria.split(":");
-            String valorCategoria = propriedadeEValorCategoria[1].trim();
-            CategoriaCardapio categoria = CategoriaCardapio.valueOf(valorCategoria);
-
-            ItemCardapio item;
-            String parteImpostoIsento = partes[7];
-            String[] propriedadeEValorImpostoIsento = parteImpostoIsento.split(":");
-            String valorImpostoIsento = propriedadeEValorImpostoIsento[1].trim();
-            boolean impostoIsento = Boolean.parseBoolean(valorImpostoIsento);
-            if (impostoIsento) {
-                item = new ItemCardapioIsento(id, nome, descricao, preco, categoria);
-            } else {
-                item = new ItemCardapio(id, nome, descricao, preco, categoria);
-            }
-
-            String parteEmPromocao = partes[5];
-            String[] propriedadeEValorEmPromocao = parteEmPromocao.split(":");
-            String valorEmPromocao = propriedadeEValorEmPromocao[1].trim();
-            boolean emPromocao = Boolean.parseBoolean(valorEmPromocao);
-            if (emPromocao) {
-                String partePrecoDesconto = partes[6];
-                String[] propriedadeEValorPrecoDesconto = partePreco.split(":");
-                String valorPrecoDesconto = propriedadeEValorPrecoDesconto[1].trim();
-                double precoDesconto = Double.parseDouble(valorPrecoDesconto);
-                item.setPromocao(precoDesconto);
-            }
-
-            itens[i] = item;
+        ItemCardapio item;
+        String parteImpostoIsento = partes[7];
+        String[] propriedadeEValorImpostoIsento = parteImpostoIsento.split(":");
+        String valorImpostoIsento = propriedadeEValorImpostoIsento[1].trim();
+        boolean impostoIsento = Boolean.parseBoolean(valorImpostoIsento);
+        if (impostoIsento) {
+            item = new ItemCardapioIsento(id, nome, descricao, preco, categoria);
+        } else {
+            item = new ItemCardapio(id, nome, descricao, preco, categoria);
         }
-        return itens;
+
+        String parteEmPromocao = partes[5];
+        String[] propriedadeEValorEmPromocao = parteEmPromocao.split(":");
+        String valorEmPromocao = propriedadeEValorEmPromocao[1].trim();
+        boolean emPromocao = Boolean.parseBoolean(valorEmPromocao);
+        if (emPromocao) {
+            String partePrecoDesconto = partes[6];
+            String[] propriedadeEValorPrecoDesconto = partePreco.split(":");
+            String valorPrecoDesconto = propriedadeEValorPrecoDesconto[1].trim();
+            double precoDesconto = Double.parseDouble(valorPrecoDesconto);
+            item.setPromocao(precoDesconto);
+        }
+        return item;
     }
+
 }
