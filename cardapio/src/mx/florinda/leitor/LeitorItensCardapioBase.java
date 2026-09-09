1cca955b51e486a4a55665945da95138afff7cc4
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 14:00:22 -0300

    feat(LeitorItensCardapioBase): cria classe abstrata com metodo processaArquivo e metodo abstrato processaLinha

---
+++ cardapio/src/mx/florinda/leitor/LeitorItensCardapioBase.java
@@ -0,0 +1,37 @@
+package mx.florinda.leitor;
+
+import mx.florinda.modelo.ItemCardapio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public abstract class LeitorItensCardapioBase implements LeitorItensCardapio{
+
+    private final String nomeArquivo;
+
+    public LeitorItensCardapioBase(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    @Override
+    public ItemCardapio[] processaArquivo() throws IOException {
+        Path arquivo = Path.of(nomeArquivo);
+        String conteudoDoArquivo = Files.readString(arquivo);
+        String[] linhasArquivo = conteudoDoArquivo.split("\n");
+
+        ItemCardapio[] itens = new ItemCardapio[linhasArquivo.length];
+
+
+        for (int i = 0; i < linhasArquivo.length; i++) {
+            String linha = linhasArquivo[i];
+
+            ItemCardapio item = processaLinha(linha);
+            itens[i] = item;
+
+        }
+        return itens;
+    }
+
+    protected abstract ItemCardapio processaLinha(String linha);
+}
