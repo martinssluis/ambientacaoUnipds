276abd8222a2d43bf9b74987833f35dfc76243c5
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 21:40:19 -0300

    feat(ItemCardapio): adiciona toString, equals e hashCode

--- cardapio/src/mx/florinda/modelo/ItemCardapio.java
+++ cardapio/src/mx/florinda/modelo/ItemCardapio.java
@@ -1,5 +1,7 @@
 package mx.florinda.modelo;
 
+import java.util.Objects;
+
 public class ItemCardapio {
 
     // atributos
@@ -70,5 +72,30 @@ public class ItemCardapio {
 
     public CategoriaCardapio getCategoria() {
         return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemCardapio{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", emPromocao=" + emPromocao +
+                ", preco=" + preco +
+                ", precoComDesconto=" + precoComDesconto +
+                ", categoria=" + categoria +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemCardapio that = (ItemCardapio) o;
+        return id == that.id && emPromocao == that.emPromocao && Double.compare(preco, that.preco) == 0 && Double.compare(precoComDesconto, that.precoComDesconto) == 0 && Objects.equals(nome, that.nome) && Objects.equals(descricao, that.descricao) && categoria == that.categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, descricao, emPromocao, preco, precoComDesconto, categoria);
     }
 }
